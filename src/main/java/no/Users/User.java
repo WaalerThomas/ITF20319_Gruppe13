aacef8c9fdbcb35f25dc63d90fe121acafab3592
Saruu1505 <saruushan1998@hotmail.com>
2023-09-28 15:52:29 +0200

    ryddet opp i testene og la til metode i user-klassen som validerer usernames

--- src/main/java/no/Users/User.java
+++ src/main/java/no/Users/User.java
@@ -17,4 +17,13 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public void validateUserName() {
+        if (this.userName.length()>12) {
+            throw new RuntimeException("Username is too long");
+        }
+        if (this.userName.isEmpty()) {
+            System.out.println("You have to type in your username");;
+        }
+    }
 }
