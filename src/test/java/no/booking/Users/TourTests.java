b31adf0aa612a8a2b3a4a442cb4ec1a691e246ed
WaalerThomas <superxgenesis@gmail.com>
2023-10-26 14:56:32 +0200

    La til en eksempel test for lagin av omvisning

---
+++ src/test/java/no/booking/Users/TourTests.java
@@ -0,0 +1,26 @@
+package no.booking.Users;
+
+import no.booking.logic.Tour;
+import no.booking.persistence.FakeDatabase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TourTests {
+
+    @Test
+    /* Guide skal kunne opprette en omvisning */
+    public void guide_can_create_a_tour() {
+        // Arrange
+        FakeDatabase database = new FakeDatabase();
+        Tour newTour = new Tour(1,"Tur til København", "Denmark", "København", "Fantastisk tur til københavn",
+                "04.04.24", 600, "København Sentrum");
+        newTour.setTourId(1);
+
+        // Act
+        // TODO: Please implement this test
+        //database.createTour(newTour);
+
+        // Assert
+        assertEquals(1, database.getTourById(1).getTourId());
+    }
+}
